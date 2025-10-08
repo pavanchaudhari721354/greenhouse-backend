2686c40b37e8a770795e9900b6add443423aa0a0
Pavan Chaudhari <pavanchaudhari7213@gmail.com>
2025-10-08 18:22:55 +0530

    Fix Flask binding for Render deployment

--- app.py
+++ app.py
@@ -75,4 +75,7 @@ def recommend_crop():
 
 # ✅ Start Flask server
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+#if __name__ == '__main__':
+ #   app.run(debug=True) */
